543b0d595f8075bbb2c76b235de4660f0ae8ae1a
Jakob Anderson <spacerockzero@users.noreply.github.com>
2021-02-02 10:29:07 -0700

    added a test

--- src/.test.js
+++ src/.test.js
@@ -1,5 +1,6 @@
 import * as lib from "./lib";
 
+// DO NOT CHANGE TESTS
 it("have correct user", () => expect(lib.user).toBe("Developer"));
 it("have correct person name", () => expect(lib.person.name).toBe("Joe"));
 it("have correct direction", () => expect(lib.direction).toBe("up"));
@@ -7,3 +8,5 @@ it("have correct verb", () => expect(lib.pastTenseVerb).toBe("ran"));
 it("have correct location", () => expect(lib.location).toBe("arcade"));
 it("have correct game", () => expect(lib.game.name).toBe("Space Invaders"));
 it("should have tax", () => expect(lib.tax).toBe(0.1));
+it("have correct game cost after tax", () => expect(lib.game.cost).toBe(0.35));
+// DO NOT CHANGE TESTS
